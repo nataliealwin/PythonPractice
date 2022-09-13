397fd9895b5b74dc2d2274f32f2784aea0cfa0a6
nataliealwin <817350@mystma.org>
2022-09-13 15:28:47 -0500

    first funct :)

---
+++ main.py
@@ -0,0 +1,17 @@
+def natsName():
+    print("natalie rayne alwin")
+
+def twoStringVariables():
+    meow = "flow"
+    glob = "slob"
+    print(meow + glob)
+
+def multThreeVariable():
+    girl = 2
+    cat = 8
+    mom =12
+    print(girl * cat * mom)
+     
+natsName()
+twoStringVariables()
+multThreeVariable()
